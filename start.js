cc9cae66de31bbb84470d4174c0a9f6372cd3dd1
Oleg Sak <oleg.sak.by@gmail.com>
2019-01-23 22:20:46 +0300

    add security

--- start.js
+++ start.js
@@ -11,6 +11,16 @@ const api = require('./api')
 const app = express()
 const jsonParser = bodyParser.json()
 
+app.use((request, response, next) => {
+  // TODO: add secret pass for phone by uid
+  if (request.ip !== '::1' && request.ip !== '::ffff:127.0.0.1') {
+    console.log('Blocked request for:', { ip: request.ip, hostname: request.hostname })
+    response.status(423).send()
+  } else {
+    next()
+  }
+})
+
 app.use(express.static(__dirname + '/public'))
 app.get('/favicon.ico', (request, response) => response.end(''))
 
