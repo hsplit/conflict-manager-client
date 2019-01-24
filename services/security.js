a90db4b1a8d3c29aa23e2770bac7703fcae979b4
Oleg Sak <oleg.sak.by@gmail.com>
2019-01-24 23:23:06 +0300

    add blocked url info for security

--- services/security.js
+++ services/security.js
@@ -1,7 +1,7 @@
 module.exports = (request, response, next) => {
-  // TODO: add secret pass for phone by uid
+  // TODO: add secret pass for phone by uid in headers token
   if (request.ip !== '::1' && request.ip !== '::ffff:127.0.0.1') {
-    console.log('Blocked request for:', { ip: request.ip, hostname: request.hostname })
+    console.log('Blocked request for:', { ip: request.ip, hostname: request.hostname, url: request.url })
     response.status(423).send()
   } else {
     next()
